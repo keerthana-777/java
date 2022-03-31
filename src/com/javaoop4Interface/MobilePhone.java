31935b1c6e714cc96a7d55cac0ad70f51db394d5
Your Name <you@example.com>
2022-03-31 15:38:36 +0530

    committed

---
+++ src/com/javaoop4Interface/MobilePhone.java
@@ -0,0 +1,34 @@
+package com.javaoop4Interface;
+
+public class MobilePhone implements ITelephone{
+
+	@Override
+	public void powerOn() {
+		System.out.println("Powering on mobile phone");
+	}
+
+	@Override
+	public void dial(int phoneNumber) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void answer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean callPhone(int phoneNumber) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isRinging() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+} 
---
+++ src/com/javaoop4Interface/testInterface.java
@@ -0,0 +1,11 @@
+package com.javaoop4Interface;
+
+public class testInterface {
+
+	public static void main(String[] args) {
+		
+		ITelephone tele = new MobilePhone();
+		tele.powerOn();
+		
+	}
+}
